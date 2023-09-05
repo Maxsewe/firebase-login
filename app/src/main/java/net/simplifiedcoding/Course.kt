ab5333bdec2ea458baae139e4b8ea0d7be2bfef2
Max <sewemaxy@gmail.com>
2023-09-05 23:18:30 +0300

    firebase

---
+++ app/src/main/java/net/simplifiedcoding/Course.kt
@@ -0,0 +1,8 @@
+package net.simplifiedcoding
+
+data class Course(
+    // on below line creating variables.
+    var courseName: String,
+    var courseDuration: String,
+    var courseDescription: String
+)
